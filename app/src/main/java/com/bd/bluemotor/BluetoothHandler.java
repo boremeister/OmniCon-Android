20e5a9d03e6fafea92119dd29c48e1e91c30c724
boremeister <boremeister@gmail.com>
2014-12-13 01:10:17 +0100

    a) new images for main activity screen b) manual test mode 3 -> manual test mode

--- app/src/main/java/com/bd/bluemotor/BluetoothHandler.java
+++ app/src/main/java/com/bd/bluemotor/BluetoothHandler.java
@@ -22,7 +22,11 @@ public class BluetoothHandler {
 	private static Set<BluetoothDevice> pairedDevices = null;
 	private static BluetoothDevice targetDevice = null;
 	private static BluetoothSocket deviceSocket = null;
-	
+
+    public BluetoothHandler(BluetoothAdapter ba){
+        btAdapter = ba;
+    }
+
 	public static void init() {
 		if(btAdapter==null)
 			btAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -50,7 +54,6 @@ public class BluetoothHandler {
         return targetDevice;
     }
 
-	
 	/**
 	 * checks if Bluetooth adapter is supported
 	 * 
@@ -65,21 +68,66 @@ public class BluetoothHandler {
 			return false;
 		}
 	}
-	
-	/**
-	 * checks if Bluetooth adapter is enabled
-	 * 
-	 * @return true/false
-	 */
-	public static boolean isBluetoothEnabled(){
-		
-		// check if BT enabled
-		if(isBluetoothSupported() && btAdapter.isEnabled()){
-			return true;
-		}else{
-			return false;
-		}
-	}
+
+    /**
+     * checks if Bluetooth adapter is supported
+     *
+     * @return true/false
+     */
+    public boolean isBluetoothSupported1(){
+
+        // check if BT supported
+        if(btAdapter!=null){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * checks if Bluetooth adapter is enabled
+     *
+     * @return true/false
+     */
+    public static boolean isBluetoothEnabled(){
+
+        // check if BT enabled
+        if(isBluetoothSupported() && btAdapter.isEnabled()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * checks if Bluetooth adapter is enabled
+     *
+     * @return true/false
+     */
+    public boolean isBluetoothEnabled1(){
+
+        // check if BT enabled
+        if(isBluetoothSupported() && btAdapter.isEnabled()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * checks if Bluetooth adapter is ready to be used
+     *
+     * @return true/false
+     */
+    public boolean isBluetoothReady(){
+
+        // check if BT enabled
+        if(isBluetoothSupported() && btAdapter.isEnabled()){
+            return true;
+        }else{
+            return false;
+        }
+    }
 	
 	/**
 	 * gets paired devices
@@ -103,9 +151,52 @@ public class BluetoothHandler {
 		}
 		
 		return pairedDevicesList;
-	}	
-	
-	/**
+	}
+
+    /**
+     * check if any paired device
+     *
+     * @return true/false
+     */
+    public boolean doPairedDevicesExist(){
+
+        if(btAdapter != null){
+            pairedDevices = btAdapter.getBondedDevices();
+
+            // If there are paired devices
+            if (pairedDevices.size() > 0) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * gets paired devices
+     *
+     * @return ArrayList of paired devices
+     */
+    public ArrayList<String> getPairedDevices1(){
+
+        if(btAdapter != null){
+            pairedDevices = btAdapter.getBondedDevices();
+            pairedDevicesList = new ArrayList<String>();
+
+            // If there are paired devices
+            if (pairedDevices.size() > 0) {
+                // Loop through paired devices
+                for (BluetoothDevice device : pairedDevices) {
+                    // Add the name and address to an array adapter
+                    pairedDevicesList.add(device.getName() + " - " + device.getAddress() + "\n");
+                }
+            }
+        }
+
+        return pairedDevicesList;
+    }
+
+    /**
 	 * checks if 'deviceName' is already paired
 	 * 
 	 * @param deviceName
@@ -132,6 +223,74 @@ public class BluetoothHandler {
 		
 		return false;
 	}
+
+    /**
+     * checks if 'deviceName' is already paired
+     *
+     * @param deviceName
+     * @return true/false
+     */
+    public boolean isTargetDevicePaired1(String deviceName){
+
+        if(btAdapter != null){
+            pairedDevices = btAdapter.getBondedDevices();
+
+            // If there are paired devices
+            if (pairedDevices.size() > 0) {
+                // Loop through paired devices
+                for (BluetoothDevice device : pairedDevices) {
+
+                    // check if deviceName already paired
+                    if(deviceName.equals(device.getName())){
+                        targetDevice = device;
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * checks if 'deviceName' is already paired
+     *
+     * @return true/false
+     */
+    public boolean establishConnection(){
+
+        try{
+            // create socket connection
+            deviceSocket = targetDevice.createInsecureRfcommSocketToServiceRecord(DEVICE_UUID);
+            Log.i(LOG_TAG, "Device socket created OK");
+        } catch (IOException e) {
+            Log.i(LOG_TAG, "Cannot create socket: " + e.toString());
+        }
+
+        try {
+
+            // connect
+            deviceSocket.connect();
+
+            // connection established OK
+            return true;
+
+        } catch (IOException e) {
+
+            Log.i(LOG_TAG, "Cannot open socket connection: " + e.toString());
+            try {
+                deviceSocket.close();
+            } catch (Exception e2) {
+                Log.i(LOG_TAG, "Cannot close socket connection: " + e.toString());
+            }
+        }
+
+        return false;
+    }
+
+    public BluetoothSocket getSocket(){
+        return deviceSocket;
+    }
 
 	public static boolean sendMsgToDevice(String message){
 		
--- app/src/main/java/com/bd/bluemotor/ManualTestActivity.java
+++ app/src/main/java/com/bd/bluemotor/ManualTestActivity.java
@@ -2,8 +2,14 @@ package com.bd.bluemotor;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.NavUtils;
 import android.util.Log;
 import android.view.Menu;
@@ -13,34 +19,130 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
 public class ManualTestActivity extends Activity {
 
 	BoreToolbox bt;
+    BluetoothHandler bh;
+    private ConnectedThread mConnectedThread;
+
 	Button btnPairedDevices, btnSend;
-	EditText etTargetDeviceName, etMessage;
-	
+	EditText etTargetDeviceName, etCommand;
+    TextView tvRespond, tvRespondLength;
+    Handler respondHandler;
+    private StringBuilder recDataString = new StringBuilder();
+
+    private static UUID DEVICE_UUID;
+    private static Set<BluetoothDevice> pairedDevices = null;
+    private static BluetoothAdapter btAdapter = null;
+    String deviceName, responseStartChar, responseEndChar;
+
+    //private static String LOG_TAG = "com.bd.bluemotor.ManualTestActivity";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_manual_test);
 		// Show the Up button in the action bar.
 		setupActionBar();
-		
-		bt = new BoreToolbox(getApplicationContext());
-		btnPairedDevices = (Button) findViewById(R.id.buttonPairedDevices);
-		btnSend = (Button) findViewById(R.id.buttonSend);
-		etTargetDeviceName = (EditText) findViewById(R.id.editTextDeviceName);
-		etMessage = (EditText) findViewById(R.id.editTextData);
-		etTargetDeviceName.setText("HC-05");
-		
-		// check bluetooth status
+
+        // read values from settings file
+        SharedPreferences settings = getSharedPreferences("OMNICON_PREF", 0);
+        String uuid = settings.getString("UUID", null);
+        DEVICE_UUID = UUID.fromString(uuid);
+        deviceName = settings.getString("DEVICE_NAME", null);
+        responseStartChar = settings.getString("RESPONSE_STARTCHAR", null);
+        responseEndChar = settings.getString("RESPONSE_ENDCHAR", null);
+
+        // UI fields
+        btnPairedDevices = (Button) findViewById(R.id.buttonPairedDevices);
+        btnSend = (Button) findViewById(R.id.buttonSend);
+        etTargetDeviceName = (EditText) findViewById(R.id.editTextDeviceName);
+        etTargetDeviceName.setText(deviceName);
+        tvRespond = (TextView) findViewById(R.id.textViewRespond);
+        tvRespondLength = (TextView) findViewById(R.id.textViewRespondLength);
+        etCommand = (EditText) findViewById(R.id.editTextCommand);
+
+        bt = new BoreToolbox(getApplicationContext());
+
+        // initialize BluetoothHandler
+        bh = new BluetoothHandler(BluetoothAdapter.getDefaultAdapter());
+
+        /*
+        * handler for receiving data from BT module
+        */
+        respondHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+
+                Bundle bundle = msg.getData();
+                String myMsg = bundle.getString("bt_response");
+
+                recDataString.append(myMsg);                                        //keep appending to string until end char
+                int endOfLineIndex = recDataString.indexOf("#");                    // determine the end-of-line
+                if (endOfLineIndex > 0) {                                           // make sure there data before ~
+                    String dataInPrint = recDataString.substring(1, endOfLineIndex);    // extract string (without start char)
+                    tvRespond.setText(dataInPrint);
+                    int dataLength = dataInPrint.length();                          //get length of data received
+                    tvRespondLength.setText("[" + String.valueOf(dataLength) + "]");
+
+                    recDataString.delete(0, recDataString.length());                    //clear all string data
+                    dataInPrint = " ";
+                }
+            }
+        };
+
+        // check bluetooth status
 		checkBluetooth();
 		
 	}
 
-	/**
-	 * Set up the {@link android.app.ActionBar}, if the API is available.
-	 */
+    @Override
+    public void onResume() {
+
+        super.onResume();
+
+        if(bh.isBluetoothReady()){
+
+            if(bh.isTargetDevicePaired1(deviceName)){
+
+                // try to establish connection
+                if(bh.establishConnection()){
+
+                    mConnectedThread = new ConnectedThread(bh.getSocket());
+                    mConnectedThread.start();
+
+                    bt.print("Connection established OK!");
+
+                }else{
+                    bt.print("Connection could not be established!");
+                }
+            }else{
+                bt.print("Target device " + deviceName + " not paired!");
+            }
+        }
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+
+        try{
+            bh.getSocket().close();
+        }catch (IOException e){
+            // empty
+        }
+    }
+
+        /**
+         * Set up the {@link android.app.ActionBar}, if the API is available.
+         */
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	private void setupActionBar() {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
@@ -77,13 +179,16 @@ public class ManualTestActivity extends Activity {
 		TextView tv;
 		tv = (TextView) findViewById(R.id.textViewPairedDevices);
 		String text = "";
-		
-		if(!BluetoothHandler.getPairedDevices().isEmpty()){
+
+        // get paired devices
+        ArrayList<String> pairedDevicesList = bh.getPairedDevices1();
+
+		if(!pairedDevicesList.isEmpty()){
 			// header text
 			text = "Name - Address\n";
 			// devices
-			for (String device : BluetoothHandler.getPairedDevices()) {
-				text += device.toString(); 
+			for (String device : pairedDevicesList) {
+				text += device.toString();
 			}
 		} else {
 			text = "No paired devices!";
@@ -93,24 +198,17 @@ public class ManualTestActivity extends Activity {
 	}
 	
 	public void sendToTarget(View view){
-		
-		// check if device is paired
-		if(BluetoothHandler.isTargetDevicePaired(etTargetDeviceName.getText().toString())){
-			
-			// send data to it
-			//Log.i("test.com.bt", "Logcat test - SendTOTarget called!");
-			
-			String msg = etMessage.getText().toString();
-			
-			// send message to device
-			if(BluetoothHandler.sendMsgToDevice(msg)){
-				bt.print("Message (" + msg + ") successfuly sent!");
-			} else {
-				bt.print("Message (" + msg + ") NOT sent!");
-			}
-		}
-		
-	}
+
+        String msg = etCommand.getText().toString();
+
+        // if command not ended with endChar, append endChar
+        char ch = msg.charAt(msg.length() - 1);
+        if(ch!=responseEndChar.charAt(0))
+            msg = msg + responseEndChar;
+
+        mConnectedThread.write(msg);
+
+    }
 	
 	public void checkBluetooth(View view){
 
@@ -119,13 +217,10 @@ public class ManualTestActivity extends Activity {
 	}
 	
 	public void checkBluetooth(){
-		
-		// initialize BluetoothHandler
-		BluetoothHandler.init();
-		
+
 		// check if BT supported and enabled
-		if(BluetoothHandler.isBluetoothSupported()){
-			if(BluetoothHandler.isBluetoothEnabled()){
+		if(bh.isBluetoothSupported1()){
+			if(bh.isBluetoothEnabled1()){
 				// everything OK
 				bt.print("BT supported and enabled!");
 				btnPairedDevices.setEnabled(true);
@@ -141,4 +236,64 @@ public class ManualTestActivity extends Activity {
 			btnSend.setEnabled(false);
 		}
 	}
+
+    /*
+    * class for thread which is used for sending to and receiving from (listening) bluetooth module
+    */
+    private class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        //creation of the connect thread
+        public ConnectedThread(BluetoothSocket socket) {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try {
+                //Create I/O streams for connection
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+                // empty
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+
+            byte[] buffer = new byte[256];
+            int bytes;
+
+            while (true) {
+                try {
+                    bytes = mmInStream.read(buffer);            //read bytes from input buffer
+                    String readMessage = new String(buffer, 0, bytes);
+
+                    // Send the obtained bytes to the activity via handler
+                    Message msg = respondHandler.obtainMessage();
+                    Bundle bundle = new Bundle();
+                    bundle.putString("bt_response", readMessage);
+                    msg.setData(bundle);
+                    respondHandler.sendMessage(msg);
+
+                } catch (IOException e) {
+                    //break;
+                }
+            }
+        }
+
+        // write method
+        public void write(String input) {
+
+            byte[] msgBuffer = input.getBytes();
+
+            try {
+                mmOutStream.write(msgBuffer);
+            } catch (IOException e) {
+                finish();
+            }
+        }
+    }
 }
