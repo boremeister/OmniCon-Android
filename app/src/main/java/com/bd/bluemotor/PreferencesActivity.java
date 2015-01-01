b22e0e00668815a83cdb0183904cb63bf6335d14
boremeister <boremeister@gmail.com>
2015-01-01 13:27:26 +0100

    a) preferences screen (not finished)

--- app/src/main/java/com/bd/bluemotor/PreferencesActivity.java
+++ app/src/main/java/com/bd/bluemotor/PreferencesActivity.java
@@ -12,8 +12,6 @@ public class PreferencesActivity extends PreferenceActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_preferences);
-
         addPreferencesFromResource(R.xml.preferences);
 
     }
